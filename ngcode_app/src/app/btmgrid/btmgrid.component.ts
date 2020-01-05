ae3790f96972a97f9fd518c5af1bb63029250d35
Anzar <mafaq@ebay.com>
2020-01-04 19:51:02 -0800

    Added cellRenderer

--- ngcode_app/src/app/btmgrid/btmgrid.component.ts
+++ ngcode_app/src/app/btmgrid/btmgrid.component.ts
@@ -6,9 +6,11 @@ import { Component } from '@angular/core';
     styleUrls: ['./btmgrid.component.scss']
 })
 export class BtmComponent {
+
     title = 'app';
 
     columnDefs = [
+        {headerName: 'Open', field: 'open', cellRenderer: this.getRenderer()},
         {headerName: 'Make', field: 'make' },
         {headerName: 'Model', field: 'model' },
         {headerName: 'Price', field: 'price'}
@@ -19,4 +21,51 @@ export class BtmComponent {
         { make: 'Ford1', model: 'Mondeo1', price: 320001 },
         { make: 'Porsche1', model: 'Boxter1', price: 720001 }
     ];
+
+ageClicked(e) {
+    console.log('AGe clicked');
+}
+
+
+ageCellRendererFunc() {
+        return '<button (click)="ageClicked(event$)">Age</button>';
+}
+
+
+getRenderer() {
+  function CellRenderer() {}
+  CellRenderer.prototype.createGui = function() {
+    var template =
+      '<span><button id="theButton">#</button><span id="theValue" style="padding-left: 4px;"></span></span>';
+    var tempDiv = document.createElement("div");
+    tempDiv.innerHTML = template;
+    this.eGui = tempDiv.firstElementChild;
+  };
+  CellRenderer.prototype.init = function(params) {
+    this.createGui();
+    this.params = params;
+    var eValue = this.eGui.querySelector("#theValue");
+    eValue.innerHTML = params.value;
+    this.eButton = this.eGui.querySelector("#theButton");
+    this.buttonClickListener = this.onButtonClicked.bind(this);
+    this.eButton.addEventListener("click", this.buttonClickListener);
+  };
+  CellRenderer.prototype.onButtonClicked = function() {
+    console.log("BUTTON CLICKED");
+    console.log("ROW DATA: "+this.params.data);
+    var startEditingParams = {
+      rowIndex: this.params.rowIndex,
+      colKey: this.params.column.getId()
+    };
+    this.params.api.startEditingCell(startEditingParams);
+  };
+  CellRenderer.prototype.getGui = function() {
+    return this.eGui;
+  };
+  CellRenderer.prototype.destroy = function() {
+    this.eButton.removeEventListener("click", this.buttonClickListener);
+  };
+  return CellRenderer;
+}
+
 }
